b4f5da95774e92fcd02d7dcd37f0f456d6d22e4a
Lily Go <lilygo7192@gmail.com>
2023-03-29 23:04:04 -0700

    Add error html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,6 +5,12 @@
  */
 
 $(() => {
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
   const createTweetElement = function(tweet) {
     const $element = 
     $(`<article class="tweet">
@@ -14,7 +20,7 @@ $(() => {
           ${tweet.user.name}</span>
           <span class="tag">${tweet.user.handle}</span>
         </div>
-        <div class="text">${tweet.content.text}</div>
+        <div class="text">${escape(tweet.content.text)}</div>
       </header>
       
       <footer>
@@ -61,13 +67,21 @@ $(() => {
 
     if (input.length <= 5) {
       event.preventDefault();
-      return alert("You cannot submit an empty tweet.");
+      let error = $(this).parent().find("#error");
+      $(error).css({"display": "flex"});
+      $(error).find(".error-text").html("You cannot submit an empty tweet!");
+      return;
     } 
 
     if (input.length > 145) {
       event.preventDefault();
-      return alert("Your tweet is too long!");
+      let error = $(this).parent().find("#error");
+      $(error).css({"display": "flex"});
+      $(error).find(".error-text").html("You are over the text limit of 140 characters!");
+      return;
     }
+
+    $(error).css({"display": "none"});
 
     $.ajax({
       method: 'POST',
