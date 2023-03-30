f39936bc0b9425f45f28390b40831f0707d12f4f
Lily Go <lilygo7192@gmail.com>
2023-03-29 20:45:15 -0700

    Implement fetching and posting with ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,60 +4,32 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
-const createTweetElement = function(tweet) {
-  const element = 
-  `<article class="tweet">
-    <header>
-      <div class="author">
-        <span class="name"><i class="fa-solid fa-cat"></i>
-        ${tweet.user.name}</span>
-        <span class="tag">${tweet.user.handle}</span>
-      </div>
-      <div class="text">${tweet.content.text}</div>
-    </header>
-    
-    <footer>
-      <div><strong>${tweet.created_at}</strong></div>
-      <div class="icons">
-        <i class="fa-solid fa-flag"></i>
-        <i class="fa-solid fa-retweet"></i>
-        <i class="fa-solid fa-heart"></i>
-      </div>
-    </footer>
-  </article>`
-  
-  return element;
-}
-
-// loops through tweets
-// calls createTweetElement for each tweet
-// takes return value and appends it to the tweets container
 $(() => {
+  const createTweetElement = function(tweet) {
+    const $element = 
+    $(`<article class="tweet">
+      <header>
+        <div class="author">
+          <span class="name"><i class="fa-solid fa-cat"></i>
+          ${tweet.user.name}</span>
+          <span class="tag">${tweet.user.handle}</span>
+        </div>
+        <div class="text">${tweet.content.text}</div>
+      </header>
+      
+      <footer>
+        <div><strong>${tweet.created_at}</strong></div>
+        <div class="icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>`)
+    
+    return $element;
+  }
+
   const renderTweets = function(tweets) {
 
     for (const tweet of tweets) {
@@ -68,7 +40,39 @@ $(() => {
     return $('#tweets-container');
   }
   
-  console.log(renderTweets(data));
+  const fetchTweets = () => {
+    $.ajax({
+      method: 'GET',
+      url: '/tweets',
+    }).then((tweets) => {
+      renderTweets(tweets);
+    });
+
+    $("tweets-container").empty();
+
+    
+  };
+
+  fetchTweets();
+
+  $("form").submit(function(event) {
+    console.log( "Handler for .submit() called." );
+    let input = ($(this).find('#tweet-text').serialize());
+
+    console.log(input);
+
+    $.ajax({
+      method: 'POST',
+      url: 'tweets',
+      data: input
+    })
+    .then((newTweet) => {
+      console.log(newTweet);
+    });
+
+    event.preventDefault();
+  })
+
 })
 
 // Test / driver code (temporary)
